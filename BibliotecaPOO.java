ca7bcf204908bf08bc028c033e0a31f2192e8f16
luisleyvag <leyva.garcia.luis.gerardo@gmail.com>
2025-04-30 23:00:25 -0600

    Clase usuario movida al fichero principal

--- BibliotecaPOO.java
+++ BibliotecaPOO.java
@@ -166,3 +166,33 @@ class Revista extends RecursoBibliografico {
                 titulo, añoPublicacion, ejemplares, issn);
     }
 }
+
+class Usuario {
+    private String nombre;
+    private String id;
+    private List<RecursoBibliografico> prestamos;
+
+    public Usuario(String nombre, String id) {
+        this.nombre = nombre;
+        this.id = id;
+        this.prestamos = new ArrayList<>();
+    }
+
+    public boolean tomarPrestado(RecursoBibliografico recurso) {
+        if (recurso.prestar()) {
+            prestamos.add(recurso);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean devolverRecurso(RecursoBibliografico recurso) {
+        if (prestamos.remove(recurso) && recurso.devolver()) {
+            return true;
+        }
+        return false;
+    }
+
+    public String getId() { return id; }
+    public List<RecursoBibliografico> getPrestamos() { return prestamos; }
+}
